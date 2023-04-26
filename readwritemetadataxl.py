12a57491d450feb491b892ddaa6a15ae9f7ed786
rperez23 <ronnie.lperez@gmail.com>
2023-04-26 01:12:31 -0700

    v002

--- readwritemetadataxl.py
+++ readwritemetadataxl.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import os
+import re
 import sys
 import openpyxl
 import warnings
@@ -21,7 +22,7 @@ inncontestantscol  = 10
 
 #open the input xl file, exit if it fails
 try:
-    workbook = openpyxl.load_workbook(filename=xlinf)
+    workbook = openpyxl.load_workbook(filename=xlinf,data_only=True)
 except:
     print("  Cannot open",xlinf)
     sys.exit(0)
@@ -29,6 +30,26 @@ except:
 if not (xlintab in workbook.sheetnames):
     print(" ",xlintab,"not in",xlinf)
     workbook.close()
+
+ws = workbook[xlintab]
+
+for i in range(0,numeps):
+
+	row = startrow + i
+
+	buzzrid = str(ws.cell(row,innbuzzridcol).value)
+	airdate = str(ws.cell(row,innairdatecol).value)
+
+	parts = buzzrid.split('_')
+
+	epnum  = parts[1].replace('EP','')
+	season = parts[2].replace('SR00','')
+	mov    = 'ThePriceIsRight_s' + season + '_e' + epnum + '_20230410.mov'
+
+
+
+
+	print(buzzrid,':',mov,':',airdate)
 
 workbook.close()
 sys.exit(0)
